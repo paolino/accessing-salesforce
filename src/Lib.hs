4a5a94b435dab5c2efc34f02e2e3440d6a039e55
paolo <paolo.veronelli@gmail.com>
2022-07-08 20:02:27 +0200

    cleanup

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,133 +1,97 @@
-{-# LANGUAGE BlockArguments, TemplateHaskell #-}
-{-# LANGUAGE RecordWildCards, OverloadedStrings #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Lib where
 
-import Network.Wreq ( getWith, defaults, manager, Response, responseBody, post, postWith, FormParam (..), Options, auth, oauth2Bearer )
-import OpenSSL.Session (context)
-import Network.HTTP.Client.OpenSSL
-    ( withOpenSSL, opensslManagerSettings )
-import Control.Lens ( (&), (.~), view, (^?), to, (?~) )
-import Data.ByteString.Lazy ( ByteString )
-import Data.Aeson (decode, Value, FromJSON, ToJSON)
-import Data.Aeson (decode, Value, decodeFileStrict')
-import Data.Text (Text)
+import Control.Lens (to, view, (&), (.~), (?~), (^?))
+import Data.Aeson (FromJSON, ToJSON, Value, decode, decodeFileStrict')
+import Data.Aeson.Lens (AsValue (_String), key)
+import Data.ByteString.Lazy (ByteString)
 import Data.Map.Monoidal.Strict (MonoidalMap)
-import Data.Semigroup (Sum (..))
-import qualified Data.Map.Monoidal.Strict as MonoidalMap
+import Data.Map.Monoidal.Strict qualified as MonoidalMap
 import Data.Map.Strict (Map)
+import Data.Semigroup (Sum (..))
+import Data.Text (Text)
+import Data.Text.Encoding qualified as Text
 import GHC.Generics (Generic)
-import Data.Aeson.Lens ( key, AsValue(_String) ) 
-import qualified Data.Text.Encoding as Text
- 
+import Network.HTTP.Client.OpenSSL
+  ( opensslManagerSettings,
+    withOpenSSL,
+  )
+import Network.Wreq
+  ( FormParam (..),
+    Options,
+    Response,
+    auth,
+    defaults,
+    getWith,
+    manager,
+    oauth2Bearer,
+    post,
+    postWith,
+    responseBody,
+  )
+import OpenSSL.Session (context)
 
-type Result = MonoidalMap Account (Sum Usage)
+type Usage = Double
 
-type Usage = Double 
-type Account = Text 
+type Account = Text
 
 type Record = Map Text Value
 
-type Token = Text 
+type Token = Text
 
 oauthApi :: String
 oauthApi = "services/oauth2/token"
 
 getToken :: Options -> IO Token
 getToken opts = do
-    Just creds <- decodeFileStrict' "creds.json"
-    response <- fmap (view responseBody) $ withOpenSSL 
-                    $ postWith opts oauthApi $ encodeCreds creds 
-    case response ^? key "access_token" . _String  of
-            Nothing -> error "no token"
-            Just txt -> pure txt 
+  Just creds <- decodeFileStrict' "creds.json"
+  response <-
+    fmap (view responseBody) $
+      withOpenSSL $
+        postWith opts oauthApi $ encodeCreds creds
+  case response ^? key "access_token" . _String of
+    Nothing -> error "no token"
+    Just txt -> pure txt
 
-getAny :: FromJSON a => Options ->  String -> IO (Maybe a)
-getAny opts  q  = decode . view responseBody <$> 
-    do withOpenSSL $ getWith opts q 
-    
-querySalesforce :: String -> IO (Maybe Value) -- IO (Maybe [Record]) -- 
+getAny :: FromJSON a => Options -> String -> IO (Maybe a)
+getAny opts q =
+  decode . view responseBody
+    <$> do withOpenSSL $ getWith opts q
+
+querySalesforce :: String -> IO (Maybe Value) -- IO (Maybe [Record]) --
 querySalesforce query = do
-    let opts = defaults & manager .~ Left (opensslManagerSettings context)
-    token <- getToken opts
-    let opts' = opts & auth ?~ oauth2Bearer (Text.encodeUtf8 token) 
-    getAny opts'  "asdnsadnkjenk3"
+  let opts = defaults & manager .~ Left (opensslManagerSettings context)
+  token <- getToken opts
+  let opts' = opts & auth ?~ oauth2Bearer (Text.encodeUtf8 token)
+  getAny opts' "asdnsadnkjenk3"
 
+type Result = MonoidalMap Account (Sum Usage)
 
- 
-
-
-computeRanking :: [Record] -> Result 
-computeRanking xs = MonoidalMap.fromList undefined -- fmap (fmap Sum)  
+computeRanking :: [Record] -> Result
+computeRanking xs = MonoidalMap.fromList undefined -- fmap (fmap Sum)
 
 data SfCreds = SfCreds
-  { username :: Text
-  , password :: Text
-  , clientId :: Text
-  , clientSecret :: Text
+  { username :: Text,
+    password :: Text,
+    clientId :: Text,
+    clientSecret :: Text
   }
   deriving (Show, Eq, Generic)
 
-instance ToJSON SfCreds 
-instance FromJSON SfCreds 
+instance ToJSON SfCreds
 
-encodeCreds ::  SfCreds -> [FormParam]
+instance FromJSON SfCreds
+
+encodeCreds :: SfCreds -> [FormParam]
 encodeCreds SfCreds {..} =
-  [ ("username" := username)
-  , ("password":= password)
-  , ("grant_type" := ("password" :: Text))
-  , ("client_id" :=  clientId)
-  , ("client_secret" := clientSecret)
+  [ ("username" := username),
+    ("password" := password),
+    ("grant_type" := ("password" :: Text)),
+    ("client_id" := clientId),
+    ("client_secret" := clientSecret)
   ]
-
-
--- clientEnv :: BaseUrl -> IO ClientEnv
--- clientEnv bu = do
---   manager <- newManager tlsManagerSettings {managerResponseTimeout = responseTimeoutMicro $ 60 * 1_000_000}
---   pure $ mkClientEnv manager bu
-
--- runEnvClientM :: BaseUrl -> ClientM a -> IO a
--- runEnvClientM bu c = do
---   ce <- clientEnv bu
---   r <- runClientM c ce
---   case r of
---     Left e -> throwM e
---     Right x -> pure x
-
--- callSFY :: MonadIO m => FilePath -> m (CallSFT m)
--- callSFY c = liftIO (decodeFileThrow $ encodeString c) >>= callSFConf
-
--- callSFConf :: MonadIO m => SfConf -> m (CallSFT m)
--- callSFConf (SfConf bu cs) = liftIO $ do
---   pbu <- parseBaseUrl $ toS bu
---   tk <- runEnvClientM pbu $ getToken $ mkCreds cs
---   pure $ CallSFT $ \f -> liftIO $ runEnvClientM pbu $ runReaderT f tk
-
--- withSF :: MonadIO m => (FilePath -> m (CallSFT m)) -> FilePath -> WithSF b -> m b
--- withSF call fp f = do
---   CallSFT sf <- call fp
---   sf f
-
--- withSFY :: MonadIO m => FilePath -> WithSF b -> m b
--- withSFY = withSF callSFY
-
--- withToken :: (Maybe Token -> ClientM a) -> WithSF a
--- withToken f = ask >>= lift . f . Just
-
-
--- type LoginApi =
---   OauthApi (ReqBody '[FormUrlEncoded] [(Text, Text)] :> Post '[JSON] Value)
-
--- loginP :: Proxy LoginApi
--- loginP = Proxy
-
-
--- data SfConf = SfConf
---   { sfUrl :: Text
---   , sfCreds :: SfCreds
---   }
---   deriving (Show, Eq, Generic)
---   deriving
---     (FromJSON)
---     via CustomJSON '[FieldLabelModifier '[CamelToSnake, StripPrefix "sf_"]] SfConf
 
